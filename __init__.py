fe3c0e365df33a80e0c497ec9577b1fb28f3e789
Grafting Rayman <156515434+GraftingRayman@users.noreply.github.com>
2025-11-12 16:31:56 +0000

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+from .border_distance_mapper import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+
+# Required for web extensions
+WEB_DIRECTORY = "."
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
